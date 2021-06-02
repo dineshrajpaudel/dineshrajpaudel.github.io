f6b553eaba5bbce8dad4f9678ee976507edbd132
dineshrajpaudel <dineshrajpaudel84@gmail.com>
2021-06-02 17:34:53 +0545

    Updated

--- Javascript/Day2/Assignment1/script.js
+++ Javascript/Day2/Assignment1/script.js
@@ -2,13 +2,14 @@ const Page_Wrapper = document.createElement('div');
 document.body.appendChild(Page_Wrapper);
 const My_Input = document.createElement('input');
 Page_Wrapper.appendChild(My_Input);
-const ul = document.createElement('ul');
-Page_Wrapper.appendChild(ul);
+
 
 
 const My_Button = document.createElement('button');
 Page_Wrapper.appendChild(My_Button);
 My_Button.innerHTML = "PRINT";
+const ul = document.createElement('ul');
+Page_Wrapper.appendChild(ul);
 
 let number = 0;
 const My_Object = {};
