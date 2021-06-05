1bd8bbc6eeff086c766883fb9b819ff6e43ad02a
dineshrajpaudel <dineshrajpaudel84@gmail.com>
2021-06-05 17:54:52 +0545

    Updated

--- Javascript/Day4/FizzBuzz/script.js
+++ Javascript/Day4/FizzBuzz/script.js
@@ -1,6 +1,7 @@
 function fizzBuzz(number) {
     for (var i = 1; i <= number; i++) {
-        if (i % 15 === 0) console.log('FizzBuzz');
+        if (i % 15 === 0 && i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz');
+        else if (i % 15 === 0) console.log('FizzBuzz');
         else if (i % 3 === 0) console.log('Fizz');
         else if (i % 5 === 0) console.log('Buzz');
         else console.log(i);
